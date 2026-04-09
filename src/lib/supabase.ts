8f1d559275126d7d2aff1f12e0764836cded0198
logicguild733-jpg <logicguild733@gmail.com>
2026-04-09 21:54:19 +0500

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,10 +1,7 @@
+// src/lib/supabase.ts
 import { createClient } from "@supabase/supabase-js";
 
-// Your Supabase project URL
 const supabaseUrl = "https://qnkxrxxwfikhrlirfleg.supabase.co";
-
-// Your Supabase publishable/anon key
 const supabaseKey = "sb_publishable_exYuiUhOVuWEyPqROu4p5A_gCWtb89S";
 
-// Initialize Supabase client
 export const supabase = createClient(supabaseUrl, supabaseKey);
