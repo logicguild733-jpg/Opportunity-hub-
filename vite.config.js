a90d1f4c1a2fe64584d9a1907f7d7839d5de07d4
logicguild733-jpg <logicguild733@gmail.com>
2026-04-10 10:47:06 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,25 @@
-import { defineConfig } from "vite";
+ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
+
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"), // use "@/file" imports
+      "@": path.resolve(__dirname, "src"),
     },
   },
+
   build: {
     outDir: "dist",
+    emptyOutDir: true,
   },
+
   server: {
     port: 3000,
   },
