b7916285413d0d41701b13033f99520066fc9f5a
logicguild733-jpg <logicguild733@gmail.com>
2026-05-04 22:44:29 +0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,18 @@
-// src/main.tsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 const rootElement = document.getElementById("root");
 if (!rootElement) throw new Error("Root element not found");
 
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
